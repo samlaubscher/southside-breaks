2f8c43bca18ea3c28bb64978e9ada3ca185db6cb
Sam Laubscher <hakaewt@protonmail.com>
2022-02-04 19:33:39 +0000

    create django app

---
+++ southside_breaks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for southside_breaks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'southside_breaks.settings')
+
+application = get_wsgi_application()
