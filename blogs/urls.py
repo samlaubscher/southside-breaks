f91b87879f707418aa0e0b2775461a87d583e2c4
Sam Laubscher <hakaewt@protonmail.com>
2022-07-08 19:26:39 +0100

    Add beginning styles to blog page

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blogs, name='blogs'),
-    path('<int:blogs_id>/', views.view_blog, name='view_blog'),
+    path('<int:blog_id>/', views.view_blog, name='view_blog'),
     path('create_blog/', views.create_blog, name='create_blog'),
     path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
     path('delete/<int:blog_id>/', views.delete_blog,
