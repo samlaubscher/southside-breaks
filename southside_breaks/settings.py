9e3e96e6e91e5b4a30414ee590172d3d00c63992
Sam Laubscher <hakaewt@protonmail.com>
2022-07-08 12:26:02 +0100

    Set up environ to use .env

--- southside_breaks/settings.py
+++ southside_breaks/settings.py
@@ -11,8 +11,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import environ
 
 from pathlib import Path
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +29,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bq$nl5vwlbpz&$+3uy^^_xkga&6kecd=#&q-ooonnvrckb)w+5'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Check for development server
+if 'DEVELOPMENT' in env:
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,8 +53,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth',
     'allauth.account',
-    'crispy_forms',
-
     'home',
     'direct_messages',
     'threads',
@@ -55,6 +63,8 @@ INSTALLED_APPS = [
     'blogs',
     'meet_the_team',
     'event_photos',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
